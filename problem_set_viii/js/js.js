205d97e9a2ca0c6e1df90a2790b4df67495e8ad6
Harshita Garg <ritu.harshita.garg@gmail.com>
2018-03-16 02:39:39 +0000

    Tab switch succesfully! (no images yet)

--- problem_set_viii/js/js.js
+++ problem_set_viii/js/js.js
@@ -1,15 +1,40 @@
-var caesar = document.getElementById('caeser');
-var vigenere = document.getElementById('vigenere');
+// var caesar = document.getElementById('caeser');
+// var vigenere = document.getElementById('vigenere');
 
-caesar.display.style = 'block';
-vigenere.style.display = 'none';
+// caesar.style.display = 'block';
+// vigenere.style.display = 'none';
 
-function julius() {
-    caesar.style.display = 'block'; alert('hail julius caesar!');
-    vigenere.style.display = 'none'; alert('*snooty voice* french people');
+// function julius() {
+//     caesar.style.display = 'block'; alert('hail julius caesar!');
+//     vigenere.style.display = 'none'; alert('*snooty voice* french people');
+// }
+
+// function dudefrancais() {
+//     vigenere.style.display = 'block'; alert('vive la france!');
+//     caesar.style.display = 'none'; alert('rome burned');
+// }
+
+function descrip(evt, cipher, button, otherbutton) {
+    var description, tab;
+    
+    description = document.getElementsByClassName('descrip');
+    for (var i = 0; i < description.length; i++){
+        description[i].style.display = "none";
+    }
+    
+    tab = document.getElementsByClassName('tab');
+    for (var i = 0; i < tab.length; i++){
+        tab[i].className = tab[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(cipher).style.display = "block";
+    evt.currentTarget.className += " active";
+    
+    document.getElementById(button).style.backgroundColor = "white";
+    document.getElementById(button).style.border = "1px inset black";
+    
+    document.getElementById(otherbutton).style.backgroundColor = "#c1c1c1";
+    document.getElementById(otherbutton).style.border = "none";
 }
 
-function dudefrancais() {
-    vigenere.style.display = 'block'; alert('vive la france!');
-    caesar.style.display = 'none'; alert('rome burned');
-}
+document.getElementById('caeser-tb').click();
