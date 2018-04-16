17b6afcb0d309fb562a29ae7947a417bc2a9e64c
Harshita Garg <ritu.harshita.garg@gmail.com>
2018-04-16 09:51:09 -0400

    What I did at home over the weekend: redid in class

--- create_task/js/js.js
+++ create_task/js/js.js
@@ -3,27 +3,33 @@ var name = "";
 
 function welcomeOnwards() {
   name = String(document.getElementById('name').value);
-  var explain = "<p>"+name+", you are a child of the royal family, loved by all. However, the better a person is in this world, the worse their counterpart down below. Unbeknownst to you, your evil twin plots in the Land Below. One horrible day, you wake to find yourself not in your own bed, warm and comfortable, but in the dark underworld. Your evil counterpart has somehow found a way into the Land Above and replaced you. Ahead of you is a long and arduous journey to make it back to the top.</p><button onclick='onwards(one)'>Onwards!</button>";
+  contents[0][0] = "<p>"+name+", you are a child of the royal family, loved by all. However, the better a person is in this world, the worse their counterpart down below. Unbeknownst to you, your evil twin plots in the Land Below. One horrible day, you wake to find yourself not in your own bed, warm and comfortable, but in the dark underworld. Your evil counterpart has somehow found a way into the Land Above and replaced you. Ahead of you is a long and arduous journey to make it back to the top.</p><button onclick='onwards(1, 0)'>Onwards!</button>";
   document.body.style.backgroundColor = "black";
   game.style.backgroundColor = "black";
   game.style.color = "white";
-  onwards(explain);
+  onwards(0, 0);
 }
 
-function onwards(page){
-  game.innerHTML = page + restart;
-  if (page == welcome){
-    game.innerHTML = page;
+function onwards(row, col){
+  if (row == welcome){
+    game.innerHTML = row;
     document.body.style.backgroundColor = "white";
     game.style.backgroundColor = "white";
     game.style.color = "black";
   }
+  game.innerHTML = contents[row][col] + restart;
 }
 
-var explain = "<p>"+name+", you are a child of the royal family, loved by all. However, the the better a person is in this world, the worse their counterpart down below. Unbeknownst to you, your evil twin plots in the Land Below. One horrible day, you wake to find yourself not in your own bed, warm and comfortable, but in the dark underworld. Your evil counterpart has somehow found a way into the Land Above and replaced you. Ahead of you is a long and arduous journey to make it back to the top.</p><button onclick='onwards(one)'>Onwards!</button>";
-var one = "<p>You trudge along, alone and hungry. You see you're in a forest, so perhaps you can find some berries, nuts, or roots, whatever those are. Story in progress.</p><button onclick='onwards(one1)'>Option 1</button><button onclick='onwards(one2)'>Option 2</button>"
-var one1 = "<p>Story in progress</p>";
-var one2 = "<p>Story in progress</p>";
+var ch = 0, pt = 0;
+var contents = [
+  ['explain'],
+  ['chapter 1', 'good choice', 'dead choice']
+];
+
+contents[0][0] = "<p>"+name+", you are a child of the royal family, loved by all. However, the the better a person is in this world, the worse their counterpart down below. Unbeknownst to you, your evil twin plots in the Land Below. One horrible day, you wake to find yourself not in your own bed, warm and comfortable, but in the dark underworld. Your evil counterpart has somehow found a way into the Land Above and replaced you. Ahead of you is a long and arduous journey to make it back to the top.</p><button onclick='onwards(1, 0)'>Onwards!</button>";
+contents[1][0] = "<p>You trudge along, alone and hungry. You see you're in a forest, so perhaps you can find some berries, nuts, or roots, whatever those are. Story in progress.</p><button onclick='onwards(1, 1)'>Option 1</button><button onclick='onwards(1, 2)'>Option 2</button>"
+contents[1][1] = "<p>Story in progress</p>";
+contents[1][2] = "<p>Story in progress</p>";
 
 var restart = "<button onclick='onwards(welcome)'>Restart</button>"
 var welcome = "<h1>Welcome!</h1><p>Welcome, player! First, before we begin your adventure, please enter your name:</p><input type='text' id='name' value='Karen'><button onclick='welcomeOnwards()'>Onwards!</button>"
