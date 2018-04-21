191b7124cf84796b196dcbc234502cbecb3bf998
Harshita Garg <ritu.harshita.garg@gmail.com>
2018-04-21 02:21:51 +0000

    Added alternative for failure

--- create_task/js/js.js
+++ create_task/js/js.js
@@ -33,6 +33,7 @@ game.style.paddingTop = marginTop + "px";
 function welcomeOnwards() {
   name = String(document.getElementById('name').value);
   contents[0][0] = "<p>"+name+", you are a child of the royal family, loved by all. However, the better a person is in this world, the worse their counterpart down below. Unbeknownst to you, your evil twin plots in the Land Below. One horrible day, you wake to find yourself not in your own bed, warm and comfortable, but in the dark underworld. Your evil counterpart has somehow found a way into the Land Above and replaced you. Ahead of you is a long and arduous journey to make it back to the top.</p><button onclick='onwards()'>Onwards!</button>";
+  contents[1][2] = "<p>Sorry "+name+", you tried.</p>";
   document.body.style.backgroundColor = "black";
   document.body.style.color = "white";
   game.innerHTML = contents[0][0] + restart;
@@ -65,6 +66,8 @@ function check() {
   var checkAnswer = answer.includes(ans);
   if (checkAnswer) {
     onwards(1);
+  } else {
+    onwards(2);
   }
 }
 
@@ -81,7 +84,7 @@ var contents = [
 contents[0][0] = "<p>"+name+", you are a child of the royal family, loved by all. However, the better a person is in this world, the worse their counterpart down below. Unbeknownst to you, your evil twin plots in the Land Below. One horrible day, you wake to find yourself not in your own bed, warm and comfortable, but in the dark underworld. Your evil counterpart has somehow found a way into the Land Above and replaced you. Ahead of you is a long and arduous journey to make it back to the top.</p><button onclick='onwards()'>Onwards!</button>";
 contents[1][0] = "<p>You trudge along, alone and hungry. You see you're in a forest, so perhaps you can find some berries, nuts, or roots, whatever those are. Suddenly, a tall figure drops down in front of you. 'I am The Riddler. I know that you are not from here. I will appear to you and ask you questions and riddles over the course of your journey. As long as you continue to answer them all correctly, you will continue. If not...' You nod your head. The Riddler begins, 'Here is your first riddle: What has 54 hearts but no other organs?'</p><input text='text' id='answer' placeholder='What is your answer?'/><button onclick='check()'>Answer</button>";
 contents[1][1] = "<p>1Story in progress</p><button onclick='onwards()'>Onwards!</button>";
-contents[1][2] = "<p>2Story in progress</p>";
+contents[1][2] = "<p>Sorry "+name+", you tried.</p>";
 
 var restart = "<button onclick='onwards(welcome)'>Restart</button>";
 var welcome = "<h1>Welcome!</h1><p>Welcome, player! First, before we begin your adventure, please enter your name:</p><input type='text' id='name' value='Karen'><button onclick='welcomeOnwards()'>Onwards!</button>";
